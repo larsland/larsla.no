fb06a0150833221f1b2101f59489e46c16dae80f
lars <lars_la@live.com>
2016-03-11 15:02:51 +0100

    Something

--- public/javascripts/sidebar.js
+++ public/javascripts/sidebar.js
@@ -1,13 +1,21 @@
 var box1 = document.getElementById('box1').style
+var box2 = document.getElementById('box"').style
 var click1 = document.getElementById('borderTop1')
+var click2 = document.getElementById('borderTop2')
 
 
 function hide1() {
 	box1.display = 'none'
 }
+function hide2() {
+	box2.display = 'none'
+}
 
 function show1() {
 	box1.display = 'block'
+}
+function show2() {
+	box2.display='block'
 }
 
 click1.addEventListener('click', function() {
@@ -18,4 +26,3 @@ click1.addEventListener('click', function() {
 		hide1()
 	}
 })
-
