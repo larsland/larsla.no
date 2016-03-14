e4337da8e44155dc394ee68505fffdd2db8c21e7
lars <lars_la@live.com>
2016-03-14 11:36:17 +0100

    kek

--- public/javascripts/sidebar.js
+++ public/javascripts/sidebar.js
@@ -1,28 +1,19 @@
-var box1 = document.getElementById('box1').style
-var box2 = document.getElementById('box"').style
-var click1 = document.getElementById('borderTop1')
-var click2 = document.getElementById('borderTop2')
+var box = document.getElementById('box').style
+var click = document.getElementById('borderTop')
 
-
-function hide1() {
-	box1.display = 'none'
-}
-function hide2() {
-	box2.display = 'none'
+function hide() {
+	box.display = 'none'
 }
 
-function show1() {
-	box1.display = 'block'
-}
-function show2() {
-	box2.display='block'
+function show() {
+	box.display = 'block'
 }
 
-click1.addEventListener('click', function() {
-	if (box1.display == 'none') {
-		show1()
+click.addEventListener('click', function() {
+	if (box.display == 'none') {
+		show()
 	}
 	else {
-		hide1()
+		hide()
 	}
 })
