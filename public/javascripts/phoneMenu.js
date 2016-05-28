d81d2589514dfc675bcac89c569c76b341412e89
lars <lars_la@live.com>
2016-05-28 15:58:53 +0200

    new header menu for phones

---
+++ public/javascripts/phoneMenu.js
@@ -0,0 +1,6 @@
+var menuBtn = document.getElementById('menu-btn');
+var menuTab = document.getElementById('menu-tab');
+
+var toggleMenu = function() {
+    menuTab.style.display = menuTab.style.display == "block" ? "none" : "block";
+}
