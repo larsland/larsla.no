8bffb8f1134afa4cc4a196c1834c6e0529237fd5
lars <lars_la@live.com>
2016-05-30 16:18:22 +0200

    some transition here

--- public/javascripts/phoneMenu.js
+++ public/javascripts/phoneMenu.js
@@ -1,6 +1,17 @@
 var menuBtn = document.getElementById('menu-btn');
 var menuTab = document.getElementById('menu-tab');
+var allMenuLinks = menuTab.childNodes;
 
 var toggleMenu = function() {
-    menuTab.style.display = menuTab.style.display == "block" ? "none" : "block";
+    menuTab.style.height = menuTab.style.height == "400px" ? "0px" : "400px";
+    var counter = 1;
+    for (var i = 0; i < allMenuLinks.length; i++) {
+
+        if (counter % 2 === 0) {
+            console.log(allMenuLinks[i])
+            allMenuLinks[i].style.opacity = allMenuLinks[i].style.opacity == "1" ? "0" : "1";
+        }
+        counter += 1
+
+    }
 }
