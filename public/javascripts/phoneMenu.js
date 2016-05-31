cdd87534edef3cb1821ef46d3cf09975a8407ff7
lars <lars_la@live.com>
2016-05-31 11:23:47 +0200

    fixed menu bug

--- public/javascripts/phoneMenu.js
+++ public/javascripts/phoneMenu.js
@@ -6,12 +6,9 @@ var toggleMenu = function() {
     menuTab.style.height = menuTab.style.height == "400px" ? "0px" : "400px";
     var counter = 1;
     for (var i = 0; i < allMenuLinks.length; i++) {
-
         if (counter % 2 === 0) {
-            console.log(allMenuLinks[i])
-            allMenuLinks[i].style.opacity = allMenuLinks[i].style.opacity == "1" ? "0" : "1";
+            allMenuLinks[i].style.display = allMenuLinks[i].style.display == "block" ? "none" : "block";
         }
         counter += 1
-
     }
 }
