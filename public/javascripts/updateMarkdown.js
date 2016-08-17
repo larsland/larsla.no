33b7c56d6f7d66895fa385ceac1feca48b6daa61
lars <lars_la@live.com>
2016-08-17 10:58:23 +0200

    Added proper formating on time posted for comments

--- public/javascripts/updateMarkdown.js
+++ public/javascripts/updateMarkdown.js
@@ -1,8 +1,14 @@
 var allArticleParagraphs = document.getElementsByName("article-content");
+var allCommentParagraphs = document.getElementsByName("comment");
 
 var updateMarkdown = function() {
     for (var i = 0; i < allArticleParagraphs.length; i++) {
         allArticleParagraphs[i].innerHTML = marked(allArticleParagraphs[i].innerHTML);
+    }
+    if (allCommentParagraphs.length != 0) {
+        for (var i = 0; i < allCommentParagraphs.length; i++) {
+            allCommentParagraphs[i].innerHTML = marked(allCommentParagraphs[i].innerHTML);
+        }
     }
 }
 
