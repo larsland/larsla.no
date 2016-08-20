6dcc5025be4b2502eb8f00d889db5766326f7180
lars <lars_la@live.com>
2016-08-20 16:26:24 +0200

    Separated header and footer in own files. Rewrote headerController to angular. Wrapper header section in headerController.

--- public/javascripts/controllers/headerController.js
+++ public/javascripts/controllers/headerController.js
@@ -1,30 +1,24 @@
-// For highlighting current page in navigation menu
-var url = window.location.href.split('/').slice(-1)[0];
-var homeBtn = document.getElementById('nav1');
-var newsBtn = document.getElementById('nav2');
+angular.module("larsla").controller("HeaderController", ["$scope", function($scope) {
 
-if (url == 'news') {
-    newsBtn.className = "navBtnCurrent"
-}
-else if (url == '') {
-    homeBtn.className = "navBtnCurrent"
-}
-else {
-    newsBtn.className = "navBtnCurrent"
-}
+    $scope.showMobileMenu = false;
 
-// For toggling navigation menu on phones
-var menuBtn = document.getElementById('menu-btn');
-var menuTab = document.getElementById('menu-tab');
-var allMenuLinks = menuTab.childNodes;
+    $scope.highlightCurrentNavButton = function() {
+        var url = window.location.href.split('/').slice(-1)[0];
+        var homeBtn = document.getElementById('nav1');
+        var newsBtn = document.getElementById('nav2');
 
-var toggleMenu = function() {
-    menuTab.style.height = menuTab.style.height == "400px" ? "0px" : "400px";
-    var counter = 1;
-    for (var i = 0; i < allMenuLinks.length; i++) {
-        if (counter % 2 === 0) {
-            allMenuLinks[i].style.display = allMenuLinks[i].style.display == "block" ? "none" : "block";
+        if (url == 'news') {
+            newsBtn.className = "navBtnCurrent"
         }
-        counter += 1
+        else if (url == '') {
+            homeBtn.className = "navBtnCurrent"
+        }
+        else {
+            newsBtn.className = "navBtnCurrent"
+        }
     }
-}
+
+    $scope.toggleMobileMenu = function() {
+        $scope.showMobileMenu = !$scope.showMobileMenu;
+    }
+}]);
